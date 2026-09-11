6ba7e0934be729ee9ef617238172ff64ff0f7dc9
AppsTDXSmart <apps@smartidiomas.edu.co>
2026-09-11 11:53:36 -0500

    Add files via upload

--- Calculadora Smart Mix/configuracion_registro.js
+++ Calculadora Smart Mix/configuracion_registro.js
@@ -2,7 +2,7 @@
   "use strict";
   global.SMART_REGISTRO_COTIZACIONES = Object.freeze({
     activo: true,
-    endpoint: "https://script.google.com/macros/s/AKfycbzv5A_gCu6J_GuXBV7x5UApQ3N30JCWf7QYcsjHU-FfHKXsuqrMeY5sRN6UlkBFvXeZ/exec",
+    endpoint: "https://script.google.com/macros/s/AKfycbzxZjzW2cKBT9_oG11Duv8mhNYM_QlJv2z_WAL3hbBpdWwkgei0Ao79KCXqgS2FlLmV/exec",
     destinoPestana: "Smart Mix",
     version: "2026-09-11"
   });
