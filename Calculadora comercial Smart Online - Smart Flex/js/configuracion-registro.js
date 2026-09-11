dd45e7d9ff865b1c2c124a19096da3577495769e
AppsTDXSmart <apps@smartidiomas.edu.co>
2026-09-11 12:14:25 -0500

    Add files via upload

--- Calculadora comercial Smart Online - Smart Flex/js/configuracion-registro.js
+++ Calculadora comercial Smart Online - Smart Flex/js/configuracion-registro.js
@@ -3,7 +3,7 @@
 
   global.SMART_REGISTRO_COTIZACIONES = Object.freeze({
     activo: true,
-    endpoint: "https://script.google.com/macros/s/AKfycbzv5A_gCu6J_GuXBV7x5UApQ3N30JCWf7QYcsjHU-FfHKXsuqrMeY5sRN6UlkBFvXeZ/exec",
+    endpoint: "https://script.google.com/macros/s/AKfycbzxZjzW2cKBT9_oG11Duv8mhNYM_QlJv2z_WAL3hbBpdWwkgei0Ao79KCXqgS2FlLmV/exec",
     destinoPestana: "Smart Online / Smart Flex",
     version: "2026-09-11"
   });
